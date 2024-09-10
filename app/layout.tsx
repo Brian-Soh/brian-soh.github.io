4b326f9fee955718a5cc9b6755bf4fb8eddbfb10
Brian Soh <brian.soh.cs@gmail.com>
2024-09-10 02:36:32 -0700

    Made background black

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body style={{backgroundColor:"black"}} className={inter.className}>
         <NavBar />
         <div className="absolute top-0 z-[-2] h-auto w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         {children}
