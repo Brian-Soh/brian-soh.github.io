b15ba53d528c2cdc7dddb769cf9930f1973655c3
Brian Soh <brian.soh.cs@gmail.com>
2024-09-10 01:20:21 -0700

    Updated overscroll

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col container mx-auto px-12 py-0 lg:px-44">
+    <main className="flex min-h-screen flex-col container mx-auto px-12 py-0 lg:px-44 overscroll-none">
       <AboutMe />
       <MyLanguages />
       <MyProjects />
