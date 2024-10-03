b4d9605cb2388a25c6b43da3d852914d3c4d0e36
Brian Soh <brian.soh.cs@gmail.com>
2024-10-03 14:48:28 -0700

    Updated NavLink

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -12,10 +12,11 @@ const NavLink = ({ href, title }: Props) => {
       to={href}
       spy={true}
       smooth={true}
-      offset={-140}
+      offset={-75}
       duration={0}
       className="block py-2 pl-3 pr-4 rounded md:p-0 hover:text-red-600"
     >
+      
       <p style={{ cursor: "pointer" }} className="text-xl font-semibold hover:text-red-600">{title}</p>
     </Link>
   );
